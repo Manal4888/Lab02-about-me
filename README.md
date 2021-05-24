57f6f11d7e6c922fc3daf98dd662c4faeb83f18c
Manal Ateyyat <manalateyyat4@gmail.com>
2021-05-24 16:09:27 +0300

    Lab02Readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Lab02-about-me
+# About Me, with a guessing game
+### I build an simple webpage with a direct and powerful purpose. It does all of the things that it accomplishes well. I should describe the purpose and functionality so those that visit my README understand the webpage
+
+## Author: Manal Ateyyat
+## License: MIT License
+## Built with: Java Script
+
+## How to use?
+### User schould ask 5 Yes/ No questions to learn about the site's owner "Me" so user can get to know me better..
+
+### Comments:  This is my first exposure to user stories
+
---
+++ js/app.js
@@ -0,0 +1,67 @@
+'use strict'
+let userName=prompt('What is your Name?')
+console.log('Welcome  '+ userName)
+alert('Welcome:  ' + userName)
+
+let edu=prompt('Am I an Enginer?')
+
+edu=edu.toLowerCase()
+if (edu == 'yes' || edu == 'y' )
+ { console.log('Yes, Im Electrical Engineer')
+   alert('Yes, Im Electrical Engineer')} 
+else if (edu == 'no' || edu == 'n') 
+{   console.log(' I am Electrical Engineer')
+    alert(' I am Electrical Engineer')
+}
+else{ console.log('Please Answer by Yes/NO or Y/N')
+      alert('Please Answer by Yes/NO or Y/N')}
+
+
+ let uni=prompt('Did I graduate from The University of Jordan?')
+
+ uni=uni.toLowerCase()
+if (uni == 'yes' || uni == 'y' )
+ {console.log('Yes, I Graduated from The University of Jordan')
+alert('Yes, I graduated from The University of Jordan')} 
+else if (uni == 'no' || uni== 'n') 
+{ console.log('Actually, I Graduated from The University of Jordan')  
+alert('Actually, I Graduated from The University of Jordan')} 
+else { console.log('Please Answer by Yes/NO or Y/N')
+alert('Please Answer by Yes/NO or Y/N')}
+
+
+ let myColor=prompt('Is Dark Blue my favorite color?')
+ myColor=myColor.toLowerCase()
+ if ( myColor == 'yes' || myColor == 'y' )
+ {console.log('Yes, Dark Blue is my favorite color')
+alert('Yes, Dark Blue my is favorite color')} 
+else if ( myColor== 'no' ||  myColor== 'n') 
+{ console.log('Actually, Dark Blue is my favorite color') 
+alert('Actually, Dark Blue my is favorite color') } 
+else { console.log('Please Answer by Yes/NO or Y/N')
+alert('Please Answer by Yes/NO or Y/N')}
+
+
+let homeCountry=prompt('Am I a Jordanian?')
+homeCountry=homeCountry.toLowerCase()
+ if ( homeCountry == 'yes' || homeCountry == 'y' )
+ {console.log('Yes, I am a Jordanian')
+alert('Yes, I am a Jordanian')} 
+else if ( homeCountry== 'no' ||  homeCountry== 'n') 
+{ console.log('Actually, I am a Jordanian') 
+alert('Actually, I am a Jordanian') } 
+else { console.log('Please Answer by Yes/NO or Y/N')
+alert('Please Answer by Yes/NO or Y/N')}
+
+ let myGerman=prompt('Can I speak German' )
+ myGerman=myGerman.toLowerCase()
+ if ( myGerman == 'yes' || myGerman == 'y' )
+ {console.log(' Actually Not yet ^_^')
+alert(' Actually Not yet ^_^')} 
+else if ( myGerman== 'no' ||  myGerman== 'n') 
+{ console.log('you are right but soon I will')  
+alert('you are right but soon I will')} 
+else { console.log('Please Answer by Yes/NO or Y/N')
+alert('Please Answer by Yes/NO or Y/N')}
+
+alert( 'Thank you   '+ userName)
