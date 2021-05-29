ef99d2a12928f7ed673d8662b724ca7a70ceabe0
Manal Ateyyat <manalateyyat4@gmail.com>
2021-05-29 15:46:03 +0300

    final Lab02-readme

--- js/app.js
+++ js/app.js
@@ -14,14 +14,14 @@ function Q1 ()
 
 edu=edu.toLowerCase()
 if (edu == 'yes' || edu == 'y' )
- { console.log('Yes, Im Electrical Engineer')
+ { 
    alert('Yes, Im Electrical Engineer')
   score++;} 
 else if (edu == 'no' || edu == 'n') 
-{   console.log(' I am Electrical Engineer');
+{  
     alert(' I am Electrical Engineer');
 }
-else{ console.log('Please Answer by Yes/NO or Y/N')
+else{ 
       alert('Please Answer by Yes/NO or Y/N');
     }
 
@@ -34,13 +34,13 @@ Q1();
 
   uni=uni.toLowerCase()
  if (uni == 'yes' || uni == 'y' )
-  {console.log('Yes, I Graduated from The University of Jordan')
+  {
  alert('Yes, I graduated from The University of Jordan')
  score++;} 
  else if (uni == 'no' || uni== 'n') 
- { console.log('Actually, I Graduated from The University of Jordan')  
+ { 
  alert('Actually, I Graduated from The University of Jordan')} 
- else { console.log('Please Answer by Yes/NO or Y/N')
+ else { 
  alert('Please Answer by Yes/NO or Y/N')}
  
  }
@@ -52,13 +52,13 @@ function Q3 ()
  let myColor=prompt('Is Dark Blue my favorite color?')
  myColor=myColor.toLowerCase()
  if ( myColor == 'yes' || myColor == 'y' )
- {console.log('Yes, Dark Blue is my favorite color')
+ {
 alert('Yes, Dark Blue my is favorite color')
 score++;} 
 else if ( myColor== 'no' ||  myColor== 'n') 
-{ console.log('Actually, Dark Blue is my favorite color') 
+{ 
 alert('Actually, Dark Blue my is favorite color') } 
-else { console.log('Please Answer by Yes/NO or Y/N')
+else { 
 alert('Please Answer by Yes/NO or Y/N')}
 
 
@@ -70,13 +70,13 @@ function Q4 ()
   let homeCountry=prompt('Am I a Jordanian?')
 homeCountry=homeCountry.toLowerCase()
  if ( homeCountry == 'yes' || homeCountry == 'y' )
- {console.log('Yes, I am a Jordanian')
+ {
 alert('Yes, I am a Jordanian')
 score++;} 
 else if ( homeCountry== 'no' ||  homeCountry== 'n') 
-{ console.log('Actually, I am a Jordanian') 
+{ 
 alert('Actually, I am a Jordanian') } 
-else { console.log('Please Answer by Yes/NO or Y/N')
+else { 
 alert('Please Answer by Yes/NO or Y/N')}
 
 }
@@ -90,13 +90,13 @@ function Q5 ()
  let myGerman=prompt('Can I speak German' )
  myGerman=myGerman.toLowerCase()
  if ( myGerman == 'yes' || myGerman == 'y' )
- {console.log(' Actually Not yet ^_^')
+ {
 alert(' Actually Not yet ^_^')} 
 else if ( myGerman== 'no' ||  myGerman== 'n') 
-{ console.log('you are right but soon I will')  
+{  
 alert('you are right but soon I will')
 score++;} 
-else { console.log('Please Answer by Yes/NO or Y/N')
+else { 
 alert('Please Answer by Yes/NO or Y/N')}
 }
 
@@ -107,20 +107,20 @@ function Q6 ()
   {
   let yearsOfExp= prompt ('How many years of experience do I have ?')
   if (yearsOfExp== 7)
-  {console.log(`Correct, I have 7 years of experience `)
+  {
    alert(`Correct, I have 7 years of experience `)
    score++;
    break;}
   
   else if (yearsOfExp > 7)
-  {console.log(` It is too high`)
+  {
   alert(`It is too high`)}
   else if (yearsOfExp < 7)
-  {console.log(` It is too low`)
+  {
   alert(`It is too low`)}
   
   if (i===3)// to make sure that we have excute this loop 4 times then alret this message
-  {console.log(`I have 7 years of experience`)
+  {
   alert(`I have 7 years of experience`)}
   
   }
@@ -143,7 +143,7 @@ for(let i=1;i<=6;i++)
   let myFavSnack= prompt('what is my favorite snack') 
    for(let j=0;j<=snacks.length;j++)
    {if (myFavSnack == snacks[j]) 
-  { console.log(`Yes you are right`)
+  {
     alert(`Yes you are right`)
     flag=true;
     score++;
@@ -155,7 +155,7 @@ for(let i=1;i<=6;i++)
    }
    
 
-console.log(`Actually my Favorite Snacks are`)
+
 alert(`Actually my Favorite Snacks are ${snacks}`)
 
 
